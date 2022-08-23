9d1a0de2c627b11026822e2d12a98b8da5a77e0c
hemie143 <hemie143@gmail.com>
2022-08-23 15:05:06 +0200

    Added search box

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,13 +1,19 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
 
-const Banner = () => {
+const Banner = ({search, setSearch}) => {
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
+          <span id="get-part">A place to get </span>
+            <input id="search-box"
+                   type="text"
+                   placeholder="What is it you truly desire?"
+                   onChange={event => setSearch(event.target.value)}
+                   value={search}
+            />
           <span> the cool stuff.</span>
         </div>
       </div>
